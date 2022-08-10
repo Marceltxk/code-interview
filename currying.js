6d1a7e777e17fe0e891fa6a0b7831408451bd230
unknown <marceltxk@gmail.com>
2022-08-10 17:17:45 +0044

    code exercise

--- currying.js
+++ currying.js
@@ -5,12 +5,10 @@ Examples:
 multiply([1, 2, 3](2)) -> [2, 4, 6]
  */
 
-
 function multiply(array, num) {
-    array = array.forEach(element => {
-        array[element] = element * num;
+    array = array.map(element => {
+        return element * num;
     });
     return array;
 }
-
 console.log(multiply([1, 2, 3], 2));
